9586da9bda60e5e3ba7842ca458dc54a38d9ffd0
Molekon <marcelomarcondesos@gmail.com>
2021-05-03 20:35:01 -0300

    c03 - Exercício sobre Strings e aspas

---
+++ aula03/aula03.py
@@ -0,0 +1,24 @@
+"""
+str - string ("Strings" são textos dentro de aspas)
+"""
+
+print("Essa é uma 'string' (str).")
+
+print('Alguma coisa')
+
+print("Aspas duplas")
+
+print("123456")
+
+print("Esse é meu \"texto\" (str).")
+
+"""
+Caracter de escape "\" permite que seja utilizado no texto o mesmo caracter 
+utilizado na abertura da string por exemplo. """
+
+print(r"Esse é meu \n (str).")
+
+""" 
+Ao inserir a letra " r " antes da aspa de abertura da string significa que nada dentro da string deve ser interpretado, 
+neste caso foi ignorado o comando \n que seria para quebra de linha. 
+"""
