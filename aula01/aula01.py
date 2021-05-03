466725f684785b148ec1708dcae1f0c5883b22b7
molekon <marcelomarcondesos@gmail.com>
2021-05-03 18:49:39 -0300

    c01 - Exercício sobre comentários no código

---
+++ aula01/aula01.py
@@ -0,0 +1,24 @@
+# Comentários
+
+# Uma linha
+
+print('Linha 1')
+
+print('Linha 2')  # Isso é um comentário
+
+# Outra linha
+
+print('Linha 3')
+
+# print('Linha 4') //Esta linha foi comentada
+
+print('Linha 5')
+
+print('Linha 6')
+
+"""
+* Comnetário de bloco
+
+- Para criar bloco de comentário deve utilizar 3 aspas duplas ou simples, lembrando somente de utilizar
+a mesma que abriu para fechar.
+"""
