63d6dab1eea7ed326423554dcb7e52916697b8ab
molekon <marcelomarcondesos@gmail.com>
2021-05-03 18:58:05 -0300

    c02 - Exercício sobre o comando 'print'

---
+++ aula02/aula02.py
@@ -0,0 +1,21 @@
+print(1234546)
+
+print('Marcelo', 'Marcondes')
+
+print('Aline', 'Soares', sep='-')
+
+""" O argumento "sep" neste caso utilizará o " - " como separador dos argumentos """
+
+print('João', 'e', 'Maria', sep='-')
+
+print('Caio', 'Soares', sep='-', end=' ##### ')
+
+"""O argumento "end" define o que será o delimitador da linha, neste caso " ##### ", por padrão quando este 
+argumento não é informado o utilizado é uma quebra de linha. """
+
+print('Ana', 'Clara')
+
+# Exercício
+
+print('215', '553', '636', sep='.', end='-')
+print('06')
