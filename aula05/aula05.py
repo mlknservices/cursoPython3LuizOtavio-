ff0f840879b20b27f952cb61cda3b8ee89fa53bf
Molekon <marcelomarcondesos@gmail.com>
2021-05-06 17:48:09 -0300

    c05 - Exercício sobre Operadores Aritméticos

---
+++ aula05/aula05.py
@@ -0,0 +1,17 @@
+# Operadores Aritméticos
+
+print('Adição 10 + 10 =', 10 + 10)
+
+print('Subtração 10 - 5 =', 10 - 5)
+
+print('Multiplicação 10 * 10 =', 10 * 10)
+
+print('Divisão 10 / 2 =', 10 / 2)
+
+print('Divisão inteirs 10.5 // 3 =', 10.5 // 3)
+
+print('Potenciação 2 ** 10 =', 2 ** 10)
+
+print('Módulo/Resto da divisão 12 % 5 =', 12 % 5)
+
+print('Alterar precedência ((5 + 3) * 10) =', ((5 + 3) * 10))
