24518383ecf0006a837a4ce0943c661278f69fc5
Molekon <marcelomarcondesos@gmail.com>
2021-05-14 12:27:57 -0300

    c09 - Exercício sobre Input: Entrada de dados do usuário

---
+++ aula09/aula09.py
@@ -0,0 +1,35 @@
+"""
+Entrada de dados
+"""
+"""
+nome = input("Qual o seu nome? ")
+
+idade = input("Qual a sua idade? ")
+
+ano_nascimento = 2021-int(idade)  # Para converter o valor da variável para um número é necessário fazer um "cast"
+
+# O valor retornado na função "input" será sempre no tipo "String"
+
+print()
+
+print(f'O usuário digitou {nome} e o tipo da variável é {type(nome)}.')
+
+print()
+
+print(f"{nome} tem {idade} anos, e nasceu em {ano_nascimento}.")
+"""
+
+numero_1 = input("Digite um número: ")
+numero_2 = input("Digite outro número: ")
+
+numero_3 = int(input("Digite um número: "))
+numero_4 = input("Digite outro número: ")
+numero_4 = int(numero_4)
+
+print()
+
+print(numero_1 + numero_2)
+
+print()
+
+print(numero_3 + numero_4)
