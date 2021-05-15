1987629a2ccd6b0d181c198a3ed2422b2937dabc
Molekon <marcelomarcondesos@gmail.com>
2021-05-15 15:47:15 -0300

    c15 - Exercício sobre Pass ou Ellipsis como placeholders

---
+++ aula15/aula15.py
@@ -0,0 +1,10 @@
+"""
+Pass ou Ellipsis como placeholders
+"""
+
+valor = False
+
+if valor:
+    pass  # Quando precisar reservar uma área para inserir o código posteriomente, podemos utilizar a função "pass" ou "..." (ellipsis)
+else:
+    print('Tchau')
